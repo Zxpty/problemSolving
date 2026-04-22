b542c07b3ad7b7cae86dc7351d6442e279706d0e
Anthony <anthony-08.2004@hotmail.com>
2026-04-21 22:39:50 -0500

    updated

---
+++ Atcoder/atcoder454/B/B.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+
+#ifdef LOCAL
+#include "debug.cpp"
+#else
+#define dbg(...)
+#endif
+#define cpu() ios::sync_with_stdio(false);cin.tie(nullptr);
+
+using namespace std;
+
+#define all(a) (a).begin(), (a).end()
+#define allr(a) (a).rbegin(), (a).rend()
+#define approx(a) fixed << setprecision(a)
+
+template <class T> void read(vector<T> &v);
+template <class F, class S> void read(pair<F, S> &p);
+template <class T, size_t Z> void read(array<T, Z> &a);
+template <class T> void read(T &x) {cin >> x;}
+template <class R, class... T> void read(R& r, T&... t){read(r); read(t...);}
+template <class T> void read(vector<T> &v) {for(auto& x : v) read(x);}
+template <class F, class S> void read(pair<F, S> &p) {read(p.first, p.second);}
+template <class T, size_t Z> void read(array<T, Z> &a) { for(auto &x : a) read(x); }
+
+template <class F, class S> void pr(const pair<F, S> &x);
+template <class T> void pr(const T &x) {cout << x;}
+template <class R, class... T> void pr(const R& r, const T&... t) {pr(r); pr(t...);}
+template <class F, class S> void pr(const pair<F, S> &x) {pr("{", x.first, ", ", x.second, "}\n");}
+void ps() {pr("\n");}
+template <class T> void ps(const T &x) {pr(x); ps();}
+template <class T> void ps(vector<T> &v) {for(auto& x : v) pr(x, ' '); ps();}
+template <class T, size_t Z> void ps(const array<T, Z> &a) { for(auto &x : a) pr(x, ' '); ps(); }
+template <class F, class S> void ps(const pair<F, S> &x) {pr(x.first, ' ', x.second); ps();}
+template <class R, class... T> void ps(const R& r,  const T &...t) {pr(r, ' '); ps(t...);}
+
+const int MX = 1e9;
+
+void ONO(){
+	int n, mx; read(n, mx);
+	vector<int> r(n); read(r);
+	set<int> st(r.begin(), r.end());
+	vector<int> freq(mx + 1);
+	for(int x : r) freq[x]++;
+	if(int(st.size()) == n){
+		ps("Yes");
+	}else{
+		ps("No");
+	}
+
+	bool ok = 1;
+	for(int i = 1; i <= mx; i++){
+		if(freq[i] == 0){
+			ok = 0;
+			break;
+		}
+	}
+	if(ok){
+		ps("Yes");
+	}else {
+		ps("No");
+	}
+
+
+}
+
+int main(){
+	cpu();
+	int t = 1;
+	//cin >> t;
+	while (t--)
+	{
+		ONO();
+	}
+	return 0;
+}
